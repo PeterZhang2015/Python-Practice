b78aba0a3c918aba077977b81788e74e59279456
PeterZhang2015 <zhangchongzheng@gmail.com>
2018-10-18 18:34:15 +1100

    Capbility of selecting destination language
    Capbility of selecting destination language.

--- selenium_google_translate.py
+++ selenium_google_translate.py
@@ -16,6 +16,15 @@ browser = webdriver.Chrome(chrome_options=options)
 browser.get(google_translate_url)
 time.sleep(5)
 
+
+dest_language='French'
+# Set destination language.
+dest_language_menu_path = "//*[@id='gt-tl-gms'][@role='listbox']"
+dest_language_menu_element = browser.find_element_by_xpath(dest_language_menu_path)
+dest_language_menu_element.send_keys(dest_language)
+time.sleep(2)
+
+
 # Find the src language text area element.
 src_text_path = '//textarea[@id="source"]'
 src_text_element = browser.find_element_by_xpath(src_text_path)
@@ -23,17 +32,21 @@ src_text_element.send_keys(language_for_translating)
 
 time.sleep(2)
 
+
+
+
+
 # Get translated language.
-dest_text_path = '//*[@id="gt-res-dir-ctr"]'
-dest_text_element = browser.find_element_by_xpath(dest_text_path)
-dest_text=dest_text_element.text
+#dest_text_path = '//*[@id="gt-res-dir-ctr"]'
+#dest_text_element = browser.find_element_by_xpath(dest_text_path)
+#dest_text=dest_text_element.text
 
 # Get source language.
-src_language_path = '//*[contains(text(), "- detected")][@role="button"]'
-src_language_element = browser.find_element_by_xpath(src_language_path)
-src_language_detected=src_language_element.text
+#src_language_path = '//*[contains(text(), "- detected")][@role="button"]'
+#src_language_element = browser.find_element_by_xpath(src_language_path)
+#src_language_detected=src_language_element.text
 
-src_language = re.search(r'(.*)( - detected)', src_language_detected).group(1)
+#src_language = re.search(r'(.*)( - detected)', src_language_detected).group(1)
 
 
-print src_language
+#print src_language
