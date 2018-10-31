06a687d2911acbcaf6c58ca9a2a43e20b80a1e9a
PeterZhang2015 <zhangchongzheng@gmail.com>
2018-10-31 12:11:35 +1100

    Use exec to excute python code with String format.
    It is able to put the variable value into a python code as string format, and executing it with exec function. Noted that the return variable should be defined in the string as well.
    Also adding input parameters protecting(whether exists) in this modification.

--- spirent_slc_example1.py
+++ spirent_slc_example1.py
@@ -1,4 +1,6 @@
 from SpirentSLC import SLC
+import sys
+
 slc = SLC.init(host='localhost:9005')
 
 project_list = slc.list()
@@ -6,8 +8,13 @@ print project_list
 
 #define parameters
 project_name = "spirent_slc"
-#session_name = "selenium_ffsp"
-response_name = "slc_windows_ping_ffrm"
+session_name = "Command_Prompt_ffsp"
+response_map_name = "slc_windows_ping_ffrm"
+
+#Check target iTest project exist.
+if project_name not in project_list:
+    print("project %s is not in project list %s" % (project_name, project_list))
+    sys.exit(0)
 
 #Open project
 project = slc.open(project_name)
@@ -21,13 +28,24 @@ print project_list
 #quickCallList = project.Command_Prompt_QC_fftc.list()
 #print quickCallList
 
+#Check target session profile exist in target iTest project.
+if session_name not in project_list:
+    print("session profile %s is not in project %s" % (session_name, project))
+    sys.exit(0)
 
 #Open target session in target project.
-session = project.Command_Prompt_ffsp.open()
-print session
+session_string = "session = project.{}.open()" .format(session_name)
+exec(session_string)
+
+#Check target response map exist in target iTest project.
+if response_map_name not in project_list:
+    print("response map %s is not in project %s" % (response_map_name, project))
+    sys.exit(0)
 
 #Use target response map for the command in target session.
-response_map = session.command('ping localhost', response_map=project.slc_windows_ping_ffrm)
+response_map_string = "response_map = session.command('ping localhost', response_map=project.{})" .format(response_map_name)
+exec(response_map_string)
+
 print response_map.queries()
 
 print response_map.prompt()
