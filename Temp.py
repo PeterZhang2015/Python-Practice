3d14ec7698a5612f94c0c309120b5bb8155ac81a
PeterZhang2015 <zhangchongzheng@gmail.com>
2018-11-01 12:23:12 +1100

    Temp

--- Temp.py
+++ Temp.py
@@ -5,16 +5,19 @@ import time
 import re
 import sys
 
-
-src_language_detected = "Chinese - detected"
-
-src_language = re.search(r'(.*)( - detected)', src_language_detected).group(0)
-
-print src_language
-
-src_language = re.search(r'(.*)( - detected)', src_language_detected).group(1)
+inputList = [5,2,7,2,1,8,3,2,4,6,7]
+inputList = " ".join([str(i) for i in inputList])
+#inputList = " ".join(map(str, inputList))
+print inputList
 
 
+#src_language_detected = "Chinese - detected"
+
+#src_language = re.search(r'(.*)( - detected)', src_language_detected).group(0)
+
+#print src_language
+
+#src_language = re.search(r'(.*)( - detected)', src_language_detected).group(1)
 
 
-print src_language
+#print src_language
