2d74ea2e0a923a83585f504f6a143c2b07673781
PeterZhang2015 <zhangchongzheng@gmail.com>
2020-01-20 10:41:32 +1100

    Add more protections and exception handling.
    Add more protections and exception handling.

--- appiumLibrary.py
+++ appiumLibrary.py
@@ -3,6 +3,41 @@ import unittest
 from appium import webdriver
 from time import sleep
 
+from selenium.common.exceptions import NoSuchElementException
+
+
+def isElement(self, identifyBy, identity):
+    '''
+    Determine whether elements exist
+    Usage:
+    isElement(By.XPATH,"//a")
+    '''
+    sleep(1)
+    flag=None
+    try:
+        if identifyBy == "id":
+            #self.driver.implicitly_wait(60)
+            self.driver.find_element_by_id(identity)
+        elif identifyBy == "xpath":
+            #self.driver.implicitly_wait(60)
+            self.driver.find_element_by_xpath(identity)
+        elif identifyBy == "class":
+            self.driver.find_element_by_class_name(identity)
+        elif identifyBy == "link text":
+            self.driver.find_element_by_link_text(identity)
+        elif identifyBy == "partial link text":
+            self.driver.find_element_by_partial_link_text(identity)
+        elif identifyBy == "name":
+            self.driver.find_element_by_name(identity)
+        elif identifyBy == "tag name":
+            self.driver.find_element_by_tag_name(identity)
+        elif identifyBy == "css selector":
+            self.driver.find_element_by_css_selector(identity)
+        flag = True
+    except NoSuchElementException as e:
+        flag = False
+    finally:
+        return flag
 
 def setUp(self, platformName, platformVersion, deviceName, appPackage, appActivity):
     desired_caps = {}
--- appium_practice2.py
+++ appium_practice2.py
@@ -1,9 +1,15 @@
 import os
 import unittest
+import appiumLibrary
+
 from appium import webdriver
 from time import sleep
  
 # Returns abs path relative to this file and not cwd
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.expected_conditions import presence_of_element_located
+from selenium.webdriver.support.wait import WebDriverWait
+
 PATH = lambda p: os.path.abspath(
     os.path.join(os.path.dirname(__file__), p)
 )
@@ -19,6 +25,8 @@ class MAndroid2Tests(unittest.TestCase):
         desired_caps['appActivity'] = '.MainActivity'
  
         self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+
+        self.wait = WebDriverWait(self.driver, 10)
  
     def tearDown(self):
         self.driver.quit()
@@ -26,40 +34,107 @@ class MAndroid2Tests(unittest.TestCase):
     def test_relogin(self):
 
         # Confirm the five permission for MAndroid2.
-        el = self.driver.find_element_by_id("com.android.packageinstaller:id/permission_allow_button");
-        el.click()
-        sleep(2)
-        e2 = self.driver.find_element_by_id("com.android.packageinstaller:id/permission_allow_button");
-        e2.click()
-        sleep(2)
-        e3 = self.driver.find_element_by_id("com.android.packageinstaller:id/permission_allow_button");
-        e3.click()
-        sleep(2)
-        e4 = self.driver.find_element_by_id("com.android.packageinstaller:id/permission_allow_button");
-        e4.click()
-        sleep(2)
-        e5 = self.driver.find_element_by_id("com.android.packageinstaller:id/permission_allow_button");
-        e5.click()
-        sleep(2)
+        permissionAllowButtonId = "com.android.packageinstaller:id/permission_allow_button"
+        if (appiumLibrary.isElement(self, "id", permissionAllowButtonId)):
+            self.driver.find_element_by_id(permissionAllowButtonId).click()
+           # sleep(2)
+        else:
+            print("Cannot find the first permission")
+            return
+
+        self.wait.until(presence_of_element_located((By.ID, permissionAllowButtonId)))
+
+        if (appiumLibrary.isElement(self, "id", permissionAllowButtonId)):
+            self.driver.find_element_by_id(permissionAllowButtonId).click()
+           # sleep(2)
+        else:
+            print("Cannot find the second permission")
+            return
+
+        self.wait.until(presence_of_element_located((By.ID, permissionAllowButtonId)))
+
+        if (appiumLibrary.isElement(self, "id", permissionAllowButtonId)):
+            self.driver.find_element_by_id(permissionAllowButtonId).click()
+           # sleep(2)
+        else:
+            print("Cannot find the third permission")
+            return
+
+        self.wait.until(presence_of_element_located((By.ID, permissionAllowButtonId)))
+
+        if (appiumLibrary.isElement(self, "id", permissionAllowButtonId)):
+            self.driver.find_element_by_id(permissionAllowButtonId).click()
+           # sleep(2)
+        else:
+            print("Cannot find the fourth permission")
+            return
+
+        self.wait.until(presence_of_element_located((By.ID, permissionAllowButtonId)))
+
+        if (appiumLibrary.isElement(self, "id", permissionAllowButtonId)):
+            self.driver.find_element_by_id(permissionAllowButtonId).click()
+           # sleep(2)
+        else:
+            print("Cannot find the fifth permission")
+            return
+
+        logoutButtonId = "com.matrium.mandroid2:id/userLogout"
+        self.wait.until(presence_of_element_located((By.ID, logoutButtonId)))
 
         # Logout.
-        logOut = self.driver.find_element_by_id("com.matrium.mandroid2:id/userLogout");
-        logOut.click()
+        logOut = self.driver.find_element_by_id(logoutButtonId)
+        if (appiumLibrary.isElement(self, "id", logoutButtonId)):
+            self.driver.find_element_by_id(logoutButtonId).click()
+           # sleep(2)
+        else:
+            print("Cannot find the logout button.")
+            return
 
-        sleep(2)
+        #sleep(2)
+
+        loginButtonId = "com.matrium.mandroid2:id/loginButton"
+        self.wait.until(presence_of_element_located((By.ID, loginButtonId)))
+
+        if (appiumLibrary.isElement(self, "id", loginButtonId)):
+            print("LogOut successfully!")
+        else:
+            print("Failed to logOut!")
 
         # Login.
-        email = self.driver.find_element_by_id("com.matrium.mandroid2:id/textEmail");
-        email.send_keys("peter.zhang@matrium.com.au");
-        sleep(2)
-        password = self.driver.find_element_by_id("com.matrium.mandroid2:id/textPwd");
-        password.send_keys("matrium123");
-        sleep(2)
+        loginMail = "peter.zhang@matrium.com.au"
+        loginPassword = "matrium123"
 
-        logIn = self.driver.find_element_by_id("com.matrium.mandroid2:id/loginButton");
-        logIn.click()
-        sleep(10)
- 
+        emailTextId = "com.matrium.mandroid2:id/textEmail"
+        if (appiumLibrary.isElement(self, "id", emailTextId)):
+            self.driver.find_element_by_id(emailTextId).send_keys(loginMail)
+           # sleep(2)
+        else:
+            print("Cannot find the email text input.")
+            return
+
+        passwordTextId = "com.matrium.mandroid2:id/textPwd"
+        if (appiumLibrary.isElement(self, "id", passwordTextId)):
+            self.driver.find_element_by_id(passwordTextId).send_keys(loginPassword)
+           # sleep(2)
+        else:
+            print("Cannot find the password text input.")
+            return
+
+        if (appiumLibrary.isElement(self, "id", loginButtonId)):
+            self.driver.find_element_by_id(loginButtonId).click()
+           # sleep(2)
+        else:
+            print("Cannot find the login button.")
+            return
+
+        self.wait.until(presence_of_element_located((By.ID, logoutButtonId)))
+
+        # Check login successfully.
+        if (appiumLibrary.isElement(self, "id", logoutButtonId)):
+            print("Login successfully!")
+        else:
+            print("Failed to login!")
+
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(MAndroid2Tests)
     unittest.TextTestRunner(verbosity=2).run(suite)
